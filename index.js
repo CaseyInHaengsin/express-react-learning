23d4fa26413b541b6043c413cc4937bfada0580d
Casey Rowley <casey.rowley@kuali.co>
2020-11-24 06:11:58 -0700

    oauth flow almost complete.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./configs/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI)
+    .catch((err) => {
+        console.log(`failed to connect: ${err}`);
+    })
 
 const app = express();
 require('./routes/authRoutes')(app);
--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../configs/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleSclientSecret,
 	callbackURL: '/auth/google/callback'
-	}, (accessToken, refreshToken, profile,done) => {
+	}, async (accessToken, refreshToken, profile, done) => {
+		let existingUser = await User.findOne({ googleId: profile.id })
+		if (existingUser){
+			done(null, existingUser);
+		} else{
+			let newUser = await new User({ googleId: profile.id, name: profile.displayName}).save();
+			done(null, newUser);
+		}
+		
 		console.log(refreshToken);
 		console.log(profile);
 		console.log(accessToken);
