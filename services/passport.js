36abf5ffc19bbeb23096974b834c2202b4027a91
Casey Rowley <casey.rowley@kuali.co>
2020-11-24 19:08:34 -0700

    using proxy in google auth.

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,9 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleSclientSecret,
-	callbackURL: '/auth/google/callback'
+	callbackURL: '/auth/google/callback',
+	proxy: true
+	
 	}, async (accessToken, refreshToken, profile, done) => {
 		let existingUser = await User.findOne({ googleId: profile.id })
 		if (existingUser){
