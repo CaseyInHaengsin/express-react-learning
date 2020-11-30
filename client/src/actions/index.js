24b14fc1881ffd1cedb2883552c94a357f4c5c32
Casey Rowley <casey.rowley@kuali.co>
2020-11-30 05:27:59 -0700

    Using an action creator to fetch user.

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => {
+    return async function(dispatch){
+        let res = await axios.get('/api/current_user');
+        dispatch({type: FETCH_USER, payload: res });
+    }
+}
+
