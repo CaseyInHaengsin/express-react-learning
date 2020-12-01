4109d7668fc8c1dedaf7fee4116e1a4510e2958c
CaseyInHaengsin <casey.rowley@kuali.co>
2020-11-30 20:07:38 -0700

    Redirecting to /surveys after login.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,44 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent () {
+    switch (this.props.auth) {
+      case null:
+        return
 
-    renderContent(){
-        switch (this.props.auth){
-            case null:
-                return "Still deciding"
-
-            case false:
-                return "i'm logged out"
-
-            default:
-                return "i'm logged in"
-
-
-        }
-    }
-    render(){
-        
+      case false:
         return (
-            <nav>
-                <div className="nav-wrapper black">
-                    <a className="left brand-logo">
-                        Emaily
-                    </a>
-                    <ul className="right">
-                        <li>
-                            {this.renderContent()}
-                        </li>
-                    </ul>
-
-                </div>
-            </nav>
+          <li>
+            <a href='/auth/google'>Login with Google</a>
+          </li>
         )
+
+      default:
+        return <li><a>Logout</a></li>
     }
+  }
+  render () {
+    return (
+      <nav>
+        <div className='nav-wrapper black'>
+          <a className='left brand-logo'>Emaily</a>
+          <ul className='right'>
+            <li>{this.renderContent()}</li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
-function mapStateToProps({ auth }){
-    return { auth };
+function mapStateToProps ({ auth }) {
+  return { auth }
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)
