78dd4fa361f891fdd73c0857ba38fe16aa3fe0dd
CaseyInHaengsin <casey.rowley@kuali.co>
2020-12-03 05:35:19 -0700

    Added routing for production and heroku-postbuild command.

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const keys = require('./configs/keys');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-require('./models/User');
-require('./services/passport');
+const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const keys = require('./configs/keys')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+require('./models/User')
+require('./services/passport')
 
-mongoose.connect(keys.mongoURI)
-    .catch((err) => {
-        console.log(`failed to connect: ${err}`);
-    })
+mongoose.connect(keys.mongoURI).catch(err => {
+  console.log(`failed to connect: ${err}`)
+})
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cookieSession({
+const app = express()
+app.use(bodyParser.json())
+app.use(
+  cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 100,
     keys: [keys.cookieKey]
-  }))
+  })
+)
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
+require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
+if (process.env.NODE_ENV === 'production'){
+    //Make sure express will serve up prod assets
+    app.use(express.static('client/build'));
+    //Express will serve index.js if route isn't found
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+const PORT = process.env.PORT || 5000
 
-  
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
+app.listen(PORT)
